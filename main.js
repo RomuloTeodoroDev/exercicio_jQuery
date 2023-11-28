7bf7c44d5b85e247d6435b0e23f06de3683051aa
Rômulo Teodoro <romulo.qteodoro@gmail.com>
2023-11-28 13:23:55 -0300

    correção eventos jQuery

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
     $('form').on('submit', function(e){
         e.preventDefault()
-        $('#button').click(
-            function(){
-                const nomeTarefa = $('#nome-tarefa').val()
-                const novaTarefa = $('ul')
-                $('<li>' + nomeTarefa + '</li>').appendTo(novaTarefa)
-                $(novaTarefa).appendTo('ul')
-                $('#nome-tarefa').val('')
-                $(novaTarefa).on('click', 'li', function(){
-                    $(this).toggleClass('checked')                  
+        $(document).ready(function (){
+            $('#button').click(
+                function(){
+                    const nomeTarefa = $('#nome-tarefa').val()
+                    const novaTarefa = $('ul')
+                    $('<li>' + nomeTarefa + '</li>').appendTo(novaTarefa)
+                    $(novaTarefa).appendTo('ul')
+                    $('#nome-tarefa').val('')
+                })
+                
+            $('ul').on('click', 'li', function(){
+                $(this).toggleClass('checked')                  
                 })
             })
         }             
